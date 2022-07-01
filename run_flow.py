b501ecf8e63959d1713ef8d39b1eabae026cfcba
nick-izu <51707671+nick-izu@users.noreply.github.com>
2022-07-01 12:32:05 +0200

    Update run_flow.py

--- run_flow.py
+++ run_flow.py
@@ -8,48 +8,57 @@ DFN.check_input()
 DFN.create_network()
 DFN.mesh_network(visual_mode=False)
 
-# JDH, this part is incorrect. Every one of these will provide values for all 
-## deterministic fractures, not just one at a time. 
-
-# variable = "aperture"
-# function = "constant"
-# params = {"mu":1*10**-3}
-# b1,perm1,T1 = DFN.generate_hydraulic_values(variable,function,params,family_id=0)
-
-# function = "constant"
-# params = {"mu":1*10**-3}
-# b2,perm2,T2 = DFN.generate_hydraulic_values(variable,function,params,family_id=0)
-
-# function = "constant"
-# params = {"mu":1*10**-3}
-# b3,perm3,T3 = DFN.generate_hydraulic_values(variable,function,params,family_id=0)
-
-# function = "constant"
-# params = {"mu":5*10**-4}
-# b4,perm4,T4 = DFN.generate_hydraulic_values(variable,function,params,family_id=0)
-
-
-## JDH This portion looks good. 
-# First we define values for the stochastic families
 porosity = 1.0
 tortuosity= 1.0
 
+Layer = 1
 variable = "transmissivity"
 function = "correlated"
 params = {"alpha":8.0*10**-9, "beta":0.8}
 b1,perm1,T1 = DFN.generate_hydraulic_values(variable,function,params,family_id=1)
 
 function = "correlated"
-params = {"alpha":2.2*10**-9, "beta":0.7}
+params = {"alpha":8.0*10**-9, "beta":0.8}
 b2,perm2,T2 = DFN.generate_hydraulic_values(variable,function,params,family_id=2)
 
 function = "correlated"
-params = {"alpha":7.0*10**-11, "beta":0.7}
+params = {"alpha":8.0*10**-9, "beta":0.8}
 b3,perm3,T3 = DFN.generate_hydraulic_values(variable,function,params,family_id=3)
+
+Layer = 2
+variable = "transmissivity"
+function = "correlated"
+params = {"alpha":2.2*10**-9, "beta":0.7}
+b4,perm4,T4 = DFN.generate_hydraulic_values(variable,function,params,family_id=1)
+
+function = "correlated"
+params = {"alpha":2.2*10**-9, "beta":0.7}
+b5,perm5,T5 = DFN.generate_hydraulic_values(variable,function,params,family_id=2)
+
+function = "correlated"
+params = {"alpha":2.2*10**-9, "beta":0.7}
+b6,perm6,T6 = DFN.generate_hydraulic_values(variable,function,params,family_id=3)
+
+Layer = 3
+variable = "transmissivity"
+function = "correlated"
+params = {"alpha":7.0*10**-11, "beta":0.7}
+b7,perm7,T7 = DFN.generate_hydraulic_values(variable,function,params,family_id=1)
+
+function = "correlated"
+params = {"alpha":7.0*10**-11, "beta":0.7}
+b8,perm8,T8 = DFN.generate_hydraulic_values(variable,function,params,family_id=2)
+
+function = "correlated"
+params = {"alpha":7.0*10**-11, "beta":0.7}
+b9,perm9,T9 = DFN.generate_hydraulic_values(variable,function,params,family_id=3)
+
+
 # Then we combine them.
-T = T1 + T2 + T3 
-b = b1 + b2 + b3
-perm = perm1 + perm2 + perm3 
+T = T1 + T2 + T3 + T4 + T5 + Y6 + T7 + T8 + T9 
+b = b1 + b2 + b3 + b4 + b4 + b6 + b7 + b8 + b9
+perm = perm1 + perm2 + perm3 + perm4 + perm5 + perm6 + perm7 + perm8 + perm9
+
 # Entires 0-3, should be zero here. 
 print(b[0:5])
 
