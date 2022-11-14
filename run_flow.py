f7a441703a0d7cb7f9142977e93871f036e57fbc
nick-izu <51707671+nick-izu@users.noreply.github.com>
2022-11-14 14:33:49 +0100

    Update run_flow.py

--- run_flow.py
+++ run_flow.py
@@ -17,47 +17,47 @@ function = "correlated"
 params = {"alpha":8.0*10**-9, "beta":0.8}
 b1,perm1,T1 = DFN.generate_hydraulic_values(variable,function,params,family_id=1)
 
-#function = "correlated"
-#params = {"alpha":8.0*10**-9, "beta":0.8}
-#b1,perm2,T2 = DFN.generate_hydraulic_values(variable,function,params,family_id=2)
-
-#function = "correlated"
-#params = {"alpha":8.0*10**-9, "beta":0.8}
-#b3,perm3,T3 = DFN.generate_hydraulic_values(variable,function,params,family_id=3)
-
-#Layer = 2
-variable = "transmissivity"
 function = "correlated"
 params = {"alpha":2.2*10**-9, "beta":0.7}
 b2,perm2,T2 = DFN.generate_hydraulic_values(variable,function,params,family_id=2)
 
-#function = "correlated"
-#params = {"alpha":2.2*10**-9, "beta":0.7}
-#b5,perm5,T5 = DFN.generate_hydraulic_values(variable,function,params,family_id=2)
-
-#function = "correlated"
-#params = {"alpha":2.2*10**-9, "beta":0.7}
-#b6,perm6,T6 = DFN.generate_hydraulic_values(variable,function,params,family_id=3)
-
-#Layer = 3
-variable = "transmissivity"
 function = "correlated"
 params = {"alpha":7.0*10**-11, "beta":0.7}
 b3,perm3,T3 = DFN.generate_hydraulic_values(variable,function,params,family_id=3)
 
-#function = "correlated"
-#params = {"alpha":7.0*10**-11, "beta":0.7}
-#b8,perm8,T8 = DFN.generate_hydraulic_values(variable,function,params,family_id=2)
+#Layer = 2
+variable = "transmissivity"
+function = "correlated"
+params = {"alpha":8.0*10**-9, "beta":0.8}
+b4,perm4,T4 = DFN.generate_hydraulic_values(variable,function,params,family_id=1)
 
-#function = "correlated"
-#params = {"alpha":7.0*10**-11, "beta":0.7}
-#b9,perm9,T9 = DFN.generate_hydraulic_values(variable,function,params,family_id=3)
+function = "correlated"
+params = {"alpha":2.2*10**-9, "beta":0.7}
+b5,perm5,T5 = DFN.generate_hydraulic_values(variable,function,params,family_id=2)
+
+function = "correlated"
+params = {"alpha":7.0*10**-11, "beta":0.7}
+b6,perm6,T6 = DFN.generate_hydraulic_values(variable,function,params,family_id=3)
+
+#Layer = 3
+variable = "transmissivity"
+function = "correlated"
+params = {"alpha":8.0*10**-9, "beta":0.8}
+b7,perm7,T7 = DFN.generate_hydraulic_values(variable,function,params,family_id=1)
+
+function = "correlated"
+params = {"alpha":2.2*10**-9, "beta":0.7}
+b8,perm8,T8 = DFN.generate_hydraulic_values(variable,function,params,family_id=2)
+
+function = "correlated"
+params = {"alpha":7.0*10**-11, "beta":0.7}
+b9,perm9,T9 = DFN.generate_hydraulic_values(variable,function,params,family_id=3)
 
 
 # Then we combine them.
-T = T1 + T2 + T3  
-b = b1 + b2 + b3 
-perm = perm1 + perm2 + perm3 
+T = T1 + T2 + T3 + T4 + T5 + T6 + T7 + T8 + T9 
+b = b1 + b2 + b3 + b4 + b5 + b6 + b7 + b8 + b9
+perm = perm1 + perm2 + perm3 + perm4 + perm5 + perm6 + perm7 + perm8 + perm9
 
 # Entires 0-3, should be zero here. 
 print(b[0:5])
@@ -79,7 +79,7 @@ perm[4] = b[4]**2/12
 perm[5] = b[5]**2/12
 
 # determine values of Transmissivity for determinsitc fractures
-mu = 8.9e-4  #dynamic viscosity of water at 20 degrees C, Pa*s
+mu = 1.0e-3  #dynamic viscosity of water at 20 degrees C, Pa*s
 g = 9.8  #gravity acceleration
 rho = 1000  # water density
 
